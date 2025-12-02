41bc54a599a6022219c1d981a4a77a5ad6e286ba
muf786 <muf786@uw.edu>
2025-12-01 23:49:43 -0800

    going back - fix

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,19 +1,19 @@
-// // src/components/firebase/config.js
-// import { initializeApp } from "firebase/app";
-// import { getFirestore } from "firebase/firestore";
+// src/components/firebase/config.js
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyC7b5P5AohsATMk-v6A_SkYO_p_QOibj0c",
-//   authDomain: "hustlexp-fly-new.firebaseapp.com",
-//   projectId: "hustlexp-fly-new",
-//   storageBucket: "hustlexp-fly-new.firebasestorage.app",
-//   messagingSenderId: "962646433394",
-//   appId: "1:962646433394:web:48221b4901ff9074152ceb",
-//   measurementId: "G-LL5J17F5V8"
-// };
+const firebaseConfig = {
+  apiKey: "AIzaSyC7b5P5AohsATMk-v6A_SkYO_p_QOibj0c",
+  authDomain: "hustlexp-fly-new.firebaseapp.com",
+  projectId: "hustlexp-fly-new",
+  storageBucket: "hustlexp-fly-new.firebasestorage.app",
+  messagingSenderId: "962646433394",
+  appId: "1:962646433394:web:48221b4901ff9074152ceb",
+  measurementId: "G-LL5J17F5V8"
+};
 
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
-// // Firestore DB
-// export const db = getFirestore(app);
+// Firestore DB
+export const db = getFirestore(app);
